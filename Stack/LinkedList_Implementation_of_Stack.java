412e9ce9a12b8b1713fe9c13b5ad923d2d2f4cd6
K123Ritesh <riteshkumarlsarai@gmail.com>
2023-10-07 10:07:13 +0530

    sECOND

--- Stack/LinkedList_Implementation_of_Stack.java
+++ Stack/LinkedList_Implementation_of_Stack.java
@@ -10,5 +10,38 @@ class Node{
 
 }
 public class LinkedList_Implementation_of_Stack {
-    
+    Node head;
+    int size;
+    LinkedList_Implementation_of_Stack(){
+        head=null;
+        size=0;
+    }
+    int size(){
+        return size;
+    }
+    boolean isEmpty(){
+        return head==null;
+    }
+    void push(int x){
+        Node temp=new Node(x);
+        temp.next=head;
+        head=temp;
+        size++;
+    }
+    int pop(){
+        if(head==null){
+            return Integer.MAX_VALUE;
+        }
+        int res=head.data;
+        head=head.next;
+        size--;
+        return res;
+    }
+    int peek(){
+        if(head==null){
+            return Integer.MAX_VALUE;
+        }
+        return head.data;
+    }
+
 }
